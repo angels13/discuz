b918d8f6f0739a69117b867185754247fb653de8
joytaobao <joy@ngmap.com>
2018-08-01 14:10:01 +0800

    archiver-config

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'ngmapco2_dzx');
+define('UC_DBPW', '19860628a');
+define('UC_DBNAME', 'ngmapco2_dzx');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', '`ngmapco2_dzx`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'gbk');
+define('UC_KEY', 'B6i5h045d5hdY08ambG0ifJ0D5V9e8p1A322s1xf02G0Y518edD6IeTaDdj4iaC3');
+define('UC_API', 'http://www.52docker.com/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
